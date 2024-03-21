00b763ed0d0310728219f87bb66941a17555eeaf
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-03-20 22:03:51 -0700

    Created all helper methods for the parser.

--- src/parsing/parser.cc
+++ src/parsing/parser.cc
@@ -8,6 +8,32 @@ namespace loxcompile {
 Parser::Parser(std::vector<Token>&& tokens):
     tokens_(std::move(tokens)) {}
 
+bool Parser::Match(const Token::TokenType& type) {
+    if (!Check(type)) return false;
+    
+    Advance();
+    return true;
+}
+
+bool Parser::Match(const std::vector<Token::TokenType>& types) {
+    for (const auto& type : types) {
+        bool match = Match(type);
+        if (!match) return false;
+    }
+
+    return true;
+}
+
+bool Parser::Check(const Token::TokenType& type) {
+    if (IsAtEnd()) return false;
+    return Peek().type_ == type;
+}
+
+Token& Parser::Advance() {
+    if (!IsAtEnd()) current_++;
+    return Previous();
+}
+
 bool Parser::IsAtEnd() {
     return Peek().type_ == Token::TokenType::END_OF;
 }
--- src/parsing/parser.h
+++ src/parsing/parser.h
@@ -13,6 +13,32 @@ class Parser {
     Parser(std::vector<Token>&& tokens);
 
  private:
+   /// @brief Checks if the current token is of the given type
+   // and consumes it if so.
+   /// @param type The `TokenType` to compare against.
+   /// @return Return true iff the provided `TokenType` matches
+   // the current `Token`'s type.
+   bool Match(const Token::TokenType& type);
+
+   /// @brief Checks if the current token matches all given
+   // types and consumes it if so.
+   /// @param type The `TokenType` to compare against.
+   /// @return Return true iff the provided `TokenType` matches
+   // the current `Token`'s type.
+   bool Match(const std::vector<Token::TokenType>& types);
+
+   /// @brief Checks if the current token is of the given type.
+   // Unlike match(), it never consumes the token, it only looks
+   // at it.
+   /// @param type The `TokenType` to compare against.
+   /// @return Return true iff the provided `TokenType` matches
+   // the current `Token`'s type.
+   bool Check(const Token::TokenType& type);
+
+   /// @brief Consumes the current token and returns it.
+   /// @return Returns a reference to the current `Token`.
+   Token& Advance();
+
    /// @brief Checks whether the `current_` pointer is at the end
    // of the token list.
    /// @return Returns true if we are at the end, false otherwise.
