d49a68378263814aad835e27f0fd0606bfc5a2fa
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-03-20 18:41:41 -0700

    Added Grouped, Literal, and Unary expressions.

--- src/parsing/expression.cc
+++ src/parsing/expression.cc
@@ -10,4 +10,13 @@ BinaryExpression::BinaryExpression(
     const Expression& right):
     left_(left), op_(std::move(op)), right_(right) {}
 
+GroupingExpression::GroupingExpression(const Expression& expression):
+    expression_(expression) {}
+
+LiteralExpression::LiteralExpression(std::unique_ptr<LoxObject> obj):
+    obj_(std::move(obj)) {}
+
+UnaryExpression::UnaryExpression(Token&& op, Expression right):
+    op_(std::move(op)), right_(right) {}
+
 }  // loxcompile
--- src/parsing/expression.h
+++ src/parsing/expression.h
@@ -2,6 +2,7 @@
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 
+#include "../common/lox-object.h"
 #include "../scanning/token.h"
 
 namespace loxcompile {
@@ -14,10 +15,31 @@ class BinaryExpression : public Expression {
         const Expression& left, Token&& op, 
         const Expression& right);
 
- private:
     const Expression left_;
     const Token op_;
     const Expression right_;
+};
+
+class GroupingExpression : public Expression {
+ public:
+    GroupingExpression(const Expression& expression);
+
+    const Expression expression_;
+};
+
+class LiteralExpression : public Expression {
+ public:
+    LiteralExpression(std::unique_ptr<LoxObject> obj);
+
+    std::unique_ptr<LoxObject> obj_;
+};
+
+class UnaryExpression : public Expression {
+ public:
+    UnaryExpression(Token&& op, Expression right);
+
+    Token op_;
+    Expression right_;
 };
 
 }  // loxcompile
